91b2a4f90276dac3be1eeb838599c1cfda3b2dac
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-06-21 13:14:25 -0300

    updated to apply the private modifier to set function of cpf property

--- src/main/kotlin/br/com/gabriel/digionebank/Pessoa.kt
+++ src/main/kotlin/br/com/gabriel/digionebank/Pessoa.kt
@@ -1,20 +1,17 @@
 package br.com.gabriel.digionebank
 
-// Outer class
+
 class Pessoa {
     var nome: String = "Gabriel"
     var cpf: String = "333.333.333-33"
-
-    // Inner Class
-    inner class Endereco { // Classe dentro de outra classe (Pessoa())
-        var rua: String = "Rua Teste"
-    }
+    private set
 }
 
 fun main() {
     val gabriel = Pessoa()
+
+    println(gabriel)
     println(gabriel.nome)
     println(gabriel.cpf)
 
-    println(gabriel.Endereco().rua)
 }

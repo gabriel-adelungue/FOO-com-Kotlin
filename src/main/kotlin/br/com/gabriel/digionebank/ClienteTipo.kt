a24f369a4ca8c4e187662f8e7d66f174d0b8898c
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-06-22 07:33:14 -0300

    created enum client type and test to it

---
+++ src/main/kotlin/br/com/gabriel/digionebank/ClienteTipo.kt
@@ -0,0 +1,7 @@
+package br.com.gabriel.digionebank
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica")
+}
+
---
+++ src/main/kotlin/br/com/gabriel/digionebank/testes/TestClienteTipoEnum.kt
@@ -0,0 +1,19 @@
+package br.com.gabriel.digionebank.testes
+
+import br.com.gabriel.digionebank.ClienteTipo
+
+fun main () {
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} - ${pj.descricao}")
+}
